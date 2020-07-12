8f8c84f7a64bc787206809081e491835d339ad5e
Balandin228 <igorek.chelovek@gmail.com>
2020-07-12 16:20:32 +0500

    repository pattern

---
+++ TestInterviewApi/TestInterviewApi/Infrastructure/Repositories/PhoneNumberRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestInterviewApi.Domain.PhoneNumber;
+
+namespace TestInterviewApi.Infrastructure.Repositories
+{
+    public class PhoneNumberRepository : Repository<PhoneNumber>
+    {
+        public PhoneNumberRepository(TestDbContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ TestInterviewApi/TestInterviewApi/Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestInterviewApi.Domain.User;
+
+namespace TestInterviewApi.Infrastructure.Repositories
+{
+    public class  UserRepository : Repository<User>
+    {
+        public UserRepository(TestDbContext context) : base(context)
+        {
+        }
+    }
+}
