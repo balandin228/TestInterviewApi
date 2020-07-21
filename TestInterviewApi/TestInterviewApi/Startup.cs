be13d326d06b384c23ebda12a98ab9817960817d
Balandin228 <igorek.chelovek@gmail.com>
2020-07-21 18:37:17 +0500

    add swagger(5 min)

--- TestInterviewApi/TestInterviewApi/Startup.cs
+++ TestInterviewApi/TestInterviewApi/Startup.cs
@@ -1,11 +1,19 @@
+using System;
+using System.IO;
+using System.Reflection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Net.Http.Headers;
+using Microsoft.OpenApi.Models;
 using TestInterviewApi.Infrastructure;
 using TestInterviewApi.Infrastructure.Repositories;
+using Swashbuckle.AspNetCore.Swagger;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace TestInterviewApi
 {
@@ -22,10 +30,23 @@ namespace TestInterviewApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddMvcCore()
+                .AddApiExplorer();
+            services.AddLogging();
+
+            services.AddAuthentication()
+                .AddCookie(options => {
+                    options.LoginPath = "/Home/Login";
+                    options.AccessDeniedPath = "/Home/Forbidden";
+                });
             services.AddDbContext<TestDbContext>(opt=> opt.UseSqlServer(Configuration.GetConnectionString("TestDbContext"),
                 x=>x.MigrationsAssembly("TestInterviewApi")));
             services.AddTransient<IUserRepository, UserRepository>();
             services.AddTransient<IPhoneNumberRepository, PhoneNumberRepository>();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Test Interview Api", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,9 +65,14 @@ namespace TestInterviewApi
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Interview Api V1");
+            });
             app.UseRouting();
 
+            app.UseAuthentication();   
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
